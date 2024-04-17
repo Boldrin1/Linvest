a21432fab58a8362e689acafdc829a78c3676b17
Boldrin1 <srzip74@gmail.com>
2024-04-17 13:52:19 -0300

    17/04

--- Linvest/PRINCIPAL/script.js
+++ Linvest/PRINCIPAL/script.js
@@ -14,17 +14,18 @@ function toggleNav() {
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
+  
     console.log(n);
   let i;
   let slides = document.getElementsByClassName("mySlides");
