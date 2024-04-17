6d66686d114f75a87b8c95cd4deabdd800dd9de4
Boldrin1 <srzip74@gmail.com>
2024-04-17 11:58:47 -0300

    17/04  -- 11:58

--- Linvest/Slideshow/slide.js
+++ Linvest/Slideshow/slide.js
@@ -1,16 +1,3 @@
-
-function toggleNav() {
-    var sidenav = document.getElementById("mySidenav");
-    if (sidenav.style.width === "250px") {
-        sidenav.style.width = "0";
-        document.getElementsByClassName("main")[0].style.marginLeft = "0";
-    } else {
-        sidenav.style.width = "250px";
-        document.getElementsByClassName("main")[0].style.marginLeft = "250px";
-    }
-}
-
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,7 +12,6 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    console.log(n);
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
